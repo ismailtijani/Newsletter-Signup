a8a757c221ddbe89e8c2f24851f981770a125934
son-of-tijani <ismailtijani10@yahoo.com>
2022-10-27 06:11:45 +0100

    Refactored the codes

--- app.js
+++ app.js
@@ -1,58 +1,59 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const request = require('request');
-const path = require('path')
-const https = require('https')
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const https = require("https");
 
 const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}))
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 //Setup static directory to serve
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/html/signup.html'))
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/html/signup.html"));
 });
 
-app.post('/', (req, res) => {
+app.post("/", (req, res) => {
   const firstName = req.body.fname;
   const lastName = req.body.lname;
-  const email = req.body.email
+  const email = req.body.email;
 
   const data = {
-    members: [{
-      email_address: email,
-      status: 'subscribed',
-      merge_fields: {
-        FNAME: firstName,
-        LNAME: lastName
-      }
-    }]
-  }
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      },
+    ],
+  };
 
   const jsonData = JSON.stringify(data);
 
   const url = "https://us21.api.mailchimp.com/3.0/lists/82e6dca5110";
   const options = {
-    method: 'post',
-    auth: 'ismail:8e2d25c9ed173fb1a80f39f3aa9aef0b-us21'
-  }
+    method: "post",
+    auth: "ismail:8e2d25c9ed173fb1a80f39f3aa9aef0b-us21",
+  };
   const request = https.request(url, options, (response) => {
-    (response.statusCode === 200) ? res.sendFile(path.join(__dirname, 'public/html/success.html')): res.sendFile(path.join(__dirname, 'public/html/failure.html'))
-    // response.on('data', (d) => {
-    //   const data = JSON.parse(d);
-    // })
+    response.statusCode === 200
+      ? res.sendFile(path.join(__dirname, "public/html/success.html"))
+      : res.sendFile(path.join(__dirname, "public/html/failure.html"));
   });
 
   request.write(jsonData);
   request.end();
+});
 
-})
+app.post("/failure", (req, res) => res.redirect("/"));
 
-app.post('/failure', (req, res) => res.redirect('/'));
-
-app.listen(port, () => console.log('Server is running on port 3000'))
+app.listen(port, () => console.log("Server is running on port " + port));
